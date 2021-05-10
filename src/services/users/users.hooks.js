67a96e2cf4c6db290bac2df9acd5fa25a361a766
Auro <000computricks@gmail.com>
2021-05-11 00:18:37 +0530

    user patch restricted

--- src/services/users/users.hooks.js
+++ src/services/users/users.hooks.js
@@ -2,6 +2,9 @@ import * as feathersAuthentication from '@feathersjs/authentication';
 import * as local from '@feathersjs/authentication-local';
 import GenerateAccessToken from './hooks/GenerateAccessToken';
 import FRequired from '../../hooks/FRequired';
+import {iff} from 'feathers-hooks-common';
+import IsUser from '../../utils/IsUser';
+import setId from '../../hooks/setId';
 
 const {authenticate} = feathersAuthentication.hooks;
 const {hashPassword, protect} = local.hooks;
@@ -9,12 +12,12 @@ const {hashPassword, protect} = local.hooks;
 export default {
     before: {
         all: [],
-        find: [ authenticate('jwt') ],
-        get: [ authenticate('jwt') ],
-        create: [ FRequired('role', 'Role is required'), hashPassword('password') ],
-        update: [ hashPassword('password'),  authenticate('jwt') ],
-        patch: [ hashPassword('password'),  authenticate('jwt') ],
-        remove: [ authenticate('jwt') ]
+        find: [authenticate('jwt')],
+        get: [authenticate('jwt')],
+        create: [FRequired('role', 'Role is required'), hashPassword('password')],
+        update: [hashPassword('password'), authenticate('jwt')],
+        patch: [hashPassword('password'), authenticate('jwt'), iff(IsUser('student'), setId())],
+        remove: [authenticate('jwt')]
     },
 
     after: {
