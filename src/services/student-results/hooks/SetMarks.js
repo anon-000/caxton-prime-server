f7f035cd9e90833e72d4ee843dca17c30be451a3
Auro <000computricks@gmail.com>
2021-06-07 23:53:38 +0530

    total marks and secured marks fixed

--- src/services/student-results/hooks/SetMarks.js
+++ src/services/student-results/hooks/SetMarks.js
@@ -6,34 +6,47 @@
  * @createdOn 06/06/21 23:45
  */
 
+function arraysEqual(a, b) {
+    if (a === b) return true;
+    if (a == null || b == null) return false;
+    if (a.length !== b.length) return false;
+
+    for (let i = 0; i < a.length; ++i) {
+        if (a[i] !== b[i]) return false;
+    }
+    return true;
+}
 
 const SetMarks = () => async (context) => {
-    const {result} = context;
-    // console.log(result);
+    const {result, app} = context;
 
     const calculateSecuredMarks = (each) => {
         let sum = 0;
         for (const studentAnswer of each.studentAnswer) {
-            if (studentAnswer.choice === studentAnswer.question.answer) {
+            if (arraysEqual(studentAnswer.question.answer, studentAnswer.choice)) {
                 sum = sum + 4;
             }
         }
         return sum;
     };
 
+    const calculateTotalMarks = async (each) => {
+        const exam = await app.service('exams').get(each.exam._id);
+        return exam.questionCount * 4;
+    };
+
     if (result.total !== undefined) {
         for (const each of result.data) {
-            console.log(each.exam);
-            each.totalMarks = each.exam.questionCount * 4;
+            each.totalMarks = await calculateTotalMarks(each);
             each.securedMarks = calculateSecuredMarks(each);
         }
     } else if (Array.isArray(result)) {
         for (const each of result) {
-            each.totalMarks = each.exam.questionCount * 4;
+            each.totalMarks = await calculateTotalMarks(each);
             each.securedMarks = calculateSecuredMarks(each);
         }
     } else {
-        context.result.totalMarks = context.result.exam.questionCount * 4;
+        context.result.totalMarks = await calculateTotalMarks(context.result);
         context.result.securedMarks = calculateSecuredMarks(context.result);
     }
 
